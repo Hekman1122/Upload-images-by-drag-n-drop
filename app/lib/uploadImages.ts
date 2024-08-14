fa1e31abd8c88f21429edd7374ab69f5bdd976f1
Hekman <yayakinetic@gmail.com>
2024-08-15 01:14:54 +0800

    upload function fix

--- app/lib/uploadImages.ts
+++ app/lib/uploadImages.ts
@@ -3,24 +3,19 @@ import FormData from "form-data";
 
 export async function uploadImages(file: File) {
   const data = new FormData();
-  data.append("image", file);
-  data.append("type", file.type);
-  data.append("title", file.name);
-  data.append("description", "This is a simple image upload in Imgur");
-  const config = {
-    method: "post",
-    maxBodyLength: Infinity,
-    url: "https://api.imgur.com/3/image",
-    headers: {
-      Authorization: `Client-ID ${process.env.NEXT_PUBLIC_IMGUR_CLIENT_ID}`,
-    },
-    data: data,
-  };
-  axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-      console.log(error);
+  let f = URL.createObjectURL(file);
+  data.append("file", f);
+  try {
+    const result = await fetch("https://api.imgur.com/3/image", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Client-ID ${process.env.NEXT_PUBLIC_IMGUR_CLIENT_ID}`,
+      },
+      body: JSON.stringify(f),
     });
+    console.log(await result.json());
+  } catch (err) {
+    console.dir(err);
+  }
 }
